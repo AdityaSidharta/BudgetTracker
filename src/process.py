369527504558b52ea9fdc98ca46e5002476c27ac
adityasidharta <aditya.sdrt@gmail.com>
2019-10-27 18:22:44 +0800

    [Adi] First iteration of process

--- src/process.py
+++ src/process.py
@@ -1,14 +1,12 @@
 import os
-
 import fire
-import pandas as pd
 
-from src.utils.directory import CONFIG_YAML_PATH, EXPENSE_YAML_PATH, INCOME_YAML_PATH
+from src.utils.directory import CONFIG_YAML_PATH, EXPENSE_YAML_PATH, INCOME_YAML_PATH, FINAL_PATH
 from src.utils.transactions import load_transaction, get_accounts, remove_accounts, remove_keywords, \
-    map_category, categorize_transaction
+    map_category, map_description, map_type, get_emoji, get_filename
 from src.utils.io import load_yaml
 
-
+# TODO: Add logger for process
 def main(year: int = None, month: int = None):
     expense_dict = load_yaml(EXPENSE_YAML_PATH)
     config_dict = load_yaml(CONFIG_YAML_PATH)
@@ -22,15 +20,24 @@ def main(year: int = None, month: int = None):
     full_df = remove_keywords(full_df, config_dict['KEYWORD_ACCOUNTS'])
 
     expense_df = full_df[full_df["amount"] < 0.0].copy().reset_index(drop=True)
+    expense_df['true_category'] = 'OTHER_EXPENSES'
+    expense_df = map_type(expense_df)
     expense_df = map_category(expense_df, expense_dict)
-    expense_df = categorize_transaction(expense_df, expense_dict)
+    expense_df = map_description(expense_df, expense_dict)
+    expense_df = get_emoji(expense_df, expense_dict)
 
     income_df = full_df[full_df["amount"] > 0].copy().reset_index(drop=True)
+    income_df['true_category'] = 'OTHER_INCOME'
+    income_df = map_type(income_df)
     income_df = map_category(income_df, income_dict)
-    income_df = categorize_transaction(income_df, income_dict)
+    income_df = map_description(income_df, income_dict)
+    income_df = get_emoji(income_df, income_dict)
 
+    expense_path = os.path.join(FINAL_PATH, get_filename('expense', year, month))
+    expense_df.to_csv(expense_path, index=False)
 
-
+    income_path = os.path.join(FINAL_PATH, get_filename('income', year, month))
+    income_df.to_csv(income_path, index=False)
 
 
 if __name__ == "__main__":
--- src/utils/transactions.py
+++ src/utils/transactions.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 from src.utils.directory import RAW_PATH
 
+
 def get_accounts(year, month):
     accounts = []
     for filename in os.listdir(RAW_PATH):
@@ -26,6 +27,7 @@ def remove_accounts(input_df, accounts):
         df = df[~df.description.apply(lambda x: account in x)]
     return df.reset_index(drop=True)
 
+
 def remove_keywords(input_df, keywords):
     df = input_df.copy()
     for keyword in keywords:
@@ -35,3 +37,40 @@ def remove_keywords(input_df, keywords):
 
 def get_filename(account_name, year, month):
     return "{}_{}_{}.csv".format(account_name, year, month)
+
+
+def map_type(input_df):
+    df = input_df.copy()
+    df.loc[df['type'] == 'INT', 'true_category'] = 'INTEREST'
+    df.loc[df['type'] == 'ATINT', 'true_category'] = 'INTEREST'
+    df.loc[df['type'] == 'AWL', 'true_category'] = 'CASH'
+    return df
+
+
+def map_category(input_df, transaction_dict):
+    df = input_df.copy()
+    for key, items in transaction_dict.items():
+        true_category = key
+        categories = items['mapping']
+        for category in categories:
+            df.loc[df['category'] == category, 'true_category'] = true_category
+    return df
+
+
+def map_description(input_df, transaction_dict):
+    df = input_df.copy()
+    for key, items in transaction_dict.items():
+        true_category = key
+        keywords = items['description']
+        for keyword in keywords:
+            df.loc[df['description'].apply(lambda x: keyword in x), 'true_category'] = true_category
+    return df
+
+def get_emoji(input_df, transaction_dict):
+    df = input_df.copy()
+    df['emoji'] = ""
+    for key, items in transaction_dict.items():
+        true_category = key
+        emoji = items['emoji']
+        df.loc[df['true_category'] == true_category, 'emoji'] = emoji
+    return df
